743a7a514de276a1fcced9c214a32531b85f239a
Alessandra Rebecchi <alessandrarebecchi1@gmail.com>
2024-09-25 10:04:54 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
-﻿using DesafioPOO.Models;
+using DesafioPOO.Models;
+
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+// FEITO
+
+Iphone i1 = new Iphone("4444-444","iPhone 14", "123456789012345", 128);
+i1.Ligar();
+i1.ReceberLigacao();
+i1.InstalarAplicativo("Claro"); 
+
+Nokia n1 = new Nokia ("5555-555", "Nokia12", "45855555888", 128);
+n1.Ligar();
+n1.ReceberLigacao();
+n1.InstalarAplicativo("Youtube");
