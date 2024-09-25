6a7729428878e850cc2c1bb95a5bd5b02b62fa50
Alessandra Rebecchi <alessandrarebecchi1@gmail.com>
2024-09-25 10:05:21 -0300

    Update Iphone.cs

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,24 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    // FEITO
+    public class Iphone : Smartphone
     {
+
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+             Numero = numero;
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
+        }
+
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        // FEITO
+        public override void InstalarAplicativo(string nomeApp)
+        {
+         Console.WriteLine("Realizando a instalação do aplicativo para Iphone...");
+        }
     }
 }
